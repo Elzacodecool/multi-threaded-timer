478c6064461af682fdc359a4b8da43bf4c3f6789
Elzacodecool <elliegolec@gmail.com>
2018-11-08 20:35:05 +0100

    Add start main thread

--- src/main/java/com/codecool/App.java
+++ src/main/java/com/codecool/App.java
@@ -2,6 +2,7 @@ package com.codecool;
 
 public class App {
     public static void main(String[] args) {
-
+        Thread thread = new Thread(new TimerContainer());
+        thread.start();
     }
 }
