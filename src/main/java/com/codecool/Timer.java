2d2a6b225eb101c0391e254e70b1c9eb4fcb1e98
Elzacodecool <elliegolec@gmail.com>
2018-11-08 12:05:51 +0100

    Init commit

---
+++ src/main/java/com/codecool/Timer.java
@@ -0,0 +1,14 @@
+package com.codecool;
+
+public class Timer implements Runnable {
+    private String name;
+
+    public Timer(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public void run() {
+
+    }
+}
---
+++ src/main/java/com/codecool/TimerContener.java
@@ -0,0 +1,16 @@
+package com.codecool;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class TimerContener implements Runnable {
+    private Map<String, Timer> timers = new HashMap<>();
+
+
+    @Override
+    public void run() {
+
+    }
+
+
+}
